b92aa52efb4afa3989bc65db31da2bd9786a97bd
Torsten Curdt <tcurdt@vafer.org>
2011-12-24 12:58:58 +0100

    use normal not just annotated tags, always have the sha, fail if dirty for release builds

--- buildinfo-git.sh
+++ buildinfo-git.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # find git
 GIT=`which git` # /usr/local/bin/git
@@ -11,19 +11,24 @@ REVISION_DEFAULT="nil"
 if [ -d ".git" ]; then
 
   # find the most recent tag
-  RELEASE=`$GIT describe --abbrev=0 2>/dev/null`
-  if [ -z "$REVISION" ]; then
+  RELEASE=`$GIT describe --abbrev=0 --tags 2>/dev/null`
+  if [ -z "$RELEASE" ]; then
+    # if empty there probably has not been a tag yet
     RELEASE=$RELEASE_DEFAULT
   fi
 
-  # first try the convenient 'describe'
-  REVISION=`$GIT describe --dirty 2>/dev/null`
+  DESCRIBE=`$GIT describe --dirty --tags 2>/dev/null`
 
-  # if empty there probably has not been a tag yet
-  if [ -z "$REVISION" ]; then
-    # just use the last commit sha then
-    REVISION="`$GIT rev-parse --short HEAD 2>/dev/null`"
+  if [ "$DESCRIBE" != "$RELEASE" ]; then
+    if [[ "$GCC_PREPROCESSOR_DEFINITIONS" != *DEBUG* ]]; then
+      echo "ERROR: Release is dirty ($DESCRIBE)"
+      exit 1
+    else
+      echo "WARNING: Build is dirty ($DESCRIBE)"
+    fi
   fi
+
+  REVISION="`$GIT rev-parse --short HEAD 2>/dev/null`"
 
   if [ -z "$REVISION" ]; then
     # still empty means there has not been a comit yet
@@ -38,6 +43,12 @@ fi
 PREFIX_FILE="$PROJECT_TEMP_DIR/Info.plist.prefix"
 
 # create the file for the preprocessor
-echo "#define BUILD_RELEASE  $RELEASE"   > $PREFIX_FILE
-echo "#define BUILD_REVISION $REVISION" >> $PREFIX_FILE
-echo "Created Info.plist prefix file $PREFIX_FILE"
+if [ -z "$PROJECT_TEMP_DIR" ]; then
+  echo "BUILD_RELEASE = $RELEASE"
+  echo "BUILD_REVISION = $REVISION"
+else
+  echo "Created Info.plist prefix file $PREFIX_FILE for $RELEASE $REVISION"
+  echo "#define BUILD_RELEASE  $RELEASE"   > $PREFIX_FILE
+  echo "#define BUILD_REVISION $REVISION" >> $PREFIX_FILE
+fi
+
