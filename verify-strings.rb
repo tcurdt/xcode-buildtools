0cf1fabca65eb86b024858ec2540cee8e213d518
Torsten Curdt <tcurdt@vafer.org>
2012-01-14 14:27:20 +0100

    refactoring, taking xib's into account

--- verify-strings.rb
+++ verify-strings.rb
@@ -42,7 +42,7 @@ class Strings
         k, v = line.scan(/"(.*)"\s*=\s*"(.*)"/).flatten
         # puts "#{@language}: #{k} = #{v}"
         if k and v
-          @translations << Translation.new(pre, lc, k, v, language, ! (line =~ /#\s+runtime\s*$/))
+          @translations << Translation.new(pre, lc, k, v, language, ! (line =~ /#\s+external\s*$/))
           pre = ''
         else
           pre += line
@@ -76,73 +76,135 @@ class Strings
   end
 end
 
-def verify(dirs)
-  # load strings files
-  strings = []
-  Dir.glob(dirs.map { |arg| File.join(arg, '/**/*.lproj/*.strings') }, 0).each do |path|
-    language = path.scan(/([^\/]*)\.lproj/).flatten.first
-    strings << Strings.new(language).load(path)
+class Strings
+  attr_reader :path, :language
+
+  def initialize(language)
+    @translations = []
+    @language = language
   end
 
-  # find the key union
-  all_keys = Set.new
-  strings.each do |s|
-    all_keys.merge(s.keys)
-  end
-
-  # check for each key in all string files
-  all_keys.each do |k|
-    translations = []
-    strings.each do |s|
-      values = s[k]
-      translations += values
-      if values.size == 0
-        # puts "WARN: key '#{k}' is missing in #{s.path}"
-        puts "%s:%d: warning: missing key '%s'" % [ s.path, 0, k ]
-        s << Translation.new('', 0, k, '', s.language)
-      elsif values.size > 1
-        # puts "ERROR: duplicate key '#{k}' in"
-        values.each do |t|
-          # puts " #{s.path}:#{t.line}"
-          puts "%s:%d: error: duplicate key '%s'" % [ s.path, t.line, k ]
+  def load(path)
+    @path = path
+    File.open(path, 'r') do |file|
+      # puts "Loading #{path}"
+      pre = ''
+      lc = 0
+      while (line = file.gets)
+        lc += 1
+        line = "/* */" if line.match(/No comment provided/)
+        next if line.match(/^\s*$/)
+        k, v = line.scan(/"(.*)"\s*=\s*"(.*)"/).flatten
+        if k and v
+          @translations << Translation.new(pre, lc, k, v, language, ! (line =~ /#\s+external\s*$/))
+          pre = ''
+        else
+          pre += line
         end
       end
     end
-
-    # puts "#{k} =>"
-    # translations.each do |t|
-    #   puts "\t#{t.language}: \t#{t.value}"
-    # end
-    # puts
+    self
   end
 
-  def keys_in_line(line)
-    return [] if line.start_with?('//')
-    return [] if line.start_with?('/*')
-    line.scan(/NSLocalizedString\(@"(.*?)",/).flatten
+  def keys
+    @translations.map { |t| t.key }
   end
 
-  ret = 0
+  def [](key)
+    @translations.select { |t| t.key == key }
+  end
 
-  # find keys from source files
-  unused_keys = Set.new(all_keys)
+  def <<(translation)
+    @translations << translation
+  end
+end
+
+
+def xib_keys(dirs)
+  keys = Set.new
+  Dir.glob(dirs.map { |arg| File.join(arg, '/**/*.xib') }, 0).each do |path|
+    File.open(path, 'r') do |file|
+      while (line = file.gets)
+        key = line.scan(/<string key="NSTitle">(.*?)<\/string>/).flatten.first
+        keys.add(key) if key
+      end
+    end
+  end
+  keys
+end
+
+def strings(dirs)
+  ret = []
+  Dir.glob(dirs.map { |arg| File.join(arg, '/**/*.lproj/*.strings') }, 0).each do |path|
+    language = path.scan(/([^\/]*)\.lproj/).flatten.first
+    strings = Strings.new(language).load(path)
+    ret << strings
+  end
+  ret
+end
+
+def strings_keys(strings)
+  keys = Set.new
+  strings.each do |s|
+    keys.merge(s.keys)
+  end
+  keys
+end
+
+def keys_in_line(line)
+  return [] if line.start_with?('//')
+  return [] if line.start_with?('/*')
+  line.scan(/NSLocalizedString\(@"(.*?)",/).flatten
+end
+
+def code_keys(dirs)
   Dir.glob(dirs.map { |arg| File.join(arg, '/**/*.m') }, 0).each do |path|
     File.open(path, 'r') do |file|
       lc = 0
       while (line = file.gets)
         lc += 1
         keys_in_line(line.strip).each do |key|
-          if all_keys.include?(key)
-            unused_keys.delete(key)
-          else
-            puts "%s:%d: error: missing key '%s'" % [ path, lc, key ]
-            # TODO for save support we should add a Translation object here
-            ret = 1
-          end
+          yield key
         end
       end
     end
   end
+end
+
+def verify(dirs)
+  ret = 0
+  strings = strings(dirs)
+  xib_keys = xib_keys(dirs)
+  keys = strings_keys(strings) + xib_keys
+
+  # check for each key in all string files
+  keys.each do |k|
+    translations = []
+    strings.each do |s|
+      values = s[k]
+      translations += values
+      if values.size == 0
+        puts "%s:%d: warning: missing key '%s'" % [ s.path, 0, k ]
+        s << Translation.new('', 0, k, '', s.language)
+      elsif values.size > 1
+        values.each do |t|
+          puts "%s:%d: error: duplicate key '%s'" % [ s.path, t.line, k ]
+        end
+      end
+    end
+  end
+
+  unused_keys = Set.new(keys)
+  code_keys(dirs) do |key|
+    if keys.include?(key)
+      unused_keys.delete(key)
+    else
+      puts "%s:%d: error: missing key '%s'" % [ path, lc, key ]
+      ret = 1
+    end
+  end
+
+  unused_keys.subtract(xib_keys)
 
   # print unused keys
   unused_keys.each do |key|
@@ -156,14 +218,16 @@ def verify(dirs)
     end
   end
 
-  # TODO extract keys from nib files
-
-  # strings.each { |s| s.save }
   return ret
 end
 
 ret = if ARGV.length == 0
-  verify(Dir.glob(File.join(ENV['PROJECT_DIR'] || ".", "*"), 0).reject { |s| [ "frameworks", "research" ].include?(File.basename(s).downcase) })
+  ignorefile = ".verifystringsignore"
+  if File.exists?(ignorefile)
+    reject = File.open(ignorefile) { |f| f.readlines }.map { |l| l.strip }
+    puts "Ingoring #{reject}"
+  end
+  verify(Dir.glob(File.join(ENV['PROJECT_DIR'] || ".", "*"), 0).reject { |s| reject.include?(File.basename(s)) })
 else
   verify(ARGV)
 end
